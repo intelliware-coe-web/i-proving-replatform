01f9dbf0c4e7f8a489a9682c01761de5c8edf39f
Anson Chung <anson.chung@gmail.com>
2019-01-04 14:24:00 -0500

    AC - updating post to send back individual posts

--- functions/posts-handler.js
+++ functions/posts-handler.js
@@ -2,35 +2,46 @@ class PostsHandler {
 
   async getPosts(req, res) {
     const browser = res.locals.browser;
-  
+    const page = await browser.newPage();
+    
     try {
-      const page = await browser.newPage();
-  
-      await page.goto('http://i-proving.com/', {waitUntil: 'networkidle2'});
-  
-      let posts = await page.evaluate((sel) => {
-        let result = [];
-        document.querySelectorAll(sel).forEach((post) => {
-          let postLink = post.getAttribute('href');
-          let postId = postLink.replace(new RegExp('/', 'g'), '_');
+      if (req.query.postId) {
 
-          result.push({
-            title: post.innerText,
-            id: postId
-          })
-        });
+        const url = req.query.postId.replace(new RegExp('_', 'g'), '/');
+
+        await page.goto(url, {waitUntil: 'networkidle2'});
+    
+        let post = await page.evaluate((sel) => {
+          return document.querySelector(sel).innerHTML;
+        }, '.entry-content')
+
+        res.status(200).send(post);
+      } else {
+        await page.goto('http://i-proving.com/', {waitUntil: 'networkidle2'});
+
+        let posts = await page.evaluate((sel) => {
+          let result = [];
+          document.querySelectorAll(sel).forEach((post) => {
+            let postLink = post.getAttribute('href');
+            let postId = postLink.replace(new RegExp('/', 'g'), '_');
+    
+            result.push({
+              title: post.innerText,
+              id: postId
+            })
+          });
+          
+          return result;
+        }, '.entry-title a');
         
-        return result;
-      }, '.entry-title a');
-
-      res.status(200).send(posts);
+        res.status(200).send(posts);
+      }
     } catch (e) {
       res.status(500).send(e.toString());
     }
   
     await browser.close();
   }
-
 }
 
 module.exports = PostsHandler;
