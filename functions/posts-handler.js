f449cc1411566b62d4b2fd215f28262af6f0c060
Anson Chung <anson.chung@gmail.com>
2019-01-04 13:40:56 -0500

    AC - generating IDs for posts

--- functions/posts-handler.js
+++ functions/posts-handler.js
@@ -3,29 +3,34 @@ class PostsHandler {
   async getPosts(req, res) {
     const browser = res.locals.browser;
   
-    const requestURL = 'http://i-proving.com/';
-  
     try {
       const page = await browser.newPage();
   
-      await page.goto(requestURL, {waitUntil: 'networkidle2'});
+      await page.goto('http://i-proving.com/', {waitUntil: 'networkidle2'});
   
-      const POST_SELECTOR = '.entry-title';
-  
-      let postTitles = await page.evaluate((sel) => {
+      let posts = await page.evaluate((sel) => {
         let result = [];
-        document.querySelectorAll(sel).forEach((post) => result.push(post.innerText));
+        document.querySelectorAll(sel).forEach((post) => {
+          let postLink = post.getAttribute('href');
+          let postId = postLink.replace(new RegExp('/', 'g'), '_');
+
+          result.push({
+            title: post.innerText,
+            id: postId
+          })
+        });
         
         return result;
-      }, POST_SELECTOR);
-  
-      res.status(200).send(postTitles);
+      }, '.entry-title a');
+
+      res.status(200).send(posts);
     } catch (e) {
       res.status(500).send(e.toString());
     }
   
     await browser.close();
   }
+
 }
 
 module.exports = PostsHandler;
