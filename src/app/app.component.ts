5e6f3dc15ad850d4d1fc96b709de9137b0e34c1f
Anson Chung <anson.chung@gmail.com>
2019-01-10 20:25:43 -0500

    AC - call firebase endpoint for post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+interface Post {
+  titie: string,
+  id: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'i-proving-upgrade';
+  posts$: Observable<Post[]>;
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit() {
+    console.log('GETTING POSTS');
+    this.posts$ = this.http.get<Post[]>("/posts");
+  }
 }
